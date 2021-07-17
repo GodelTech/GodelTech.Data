fd625f7aa40a8dce0950478c729cb3710bc13b1e
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-07-17 15:28:47 +0300

    solve Warning SYSLIB0011: 'BinaryFormatter.Deserialize(Stream)' is obsolete: 'BinaryFormatter serialization is obsolete and should not be used. See https://aka.ms/binaryformatter for more information.

--- src/GodelTech.Data/Exceptions/DataStorageException.cs
+++ src/GodelTech.Data/Exceptions/DataStorageException.cs
@@ -10,17 +10,6 @@ namespace GodelTech.Data
     public class DataStorageException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="Exception"></see> class with serialized data.
-        /// </summary>
-        /// <param name="info">The <see cref="SerializationInfo"></see>.</param>
-        /// <param name="context">The <see cref="StreamingContext"></see>.</param>
-        protected DataStorageException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-
-        }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="DataStorageException"/> class.
         /// </summary>
         public DataStorageException()
@@ -45,6 +34,17 @@ namespace GodelTech.Data
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (<see langword="Nothing" /> in Visual Basic) if no inner exception is specified.</param>
         public DataStorageException(string message, Exception innerException)
             : base(message, innerException)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Exception"></see> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"></see>.</param>
+        /// <param name="context">The <see cref="StreamingContext"></see>.</param>
+        protected DataStorageException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
 
         }
