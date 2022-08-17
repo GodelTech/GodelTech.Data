ce246415541836b16b81e176e21879753fc984ab
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-08-17 19:01:00 +0200

    solve warnings

--- src/GodelTech.Data/Specifications/CompositeSpecification.cs
+++ src/GodelTech.Data/Specifications/CompositeSpecification.cs
@@ -3,11 +3,13 @@ using System.Linq.Expressions;
 
 namespace GodelTech.Data.Specifications
 {
+#pragma warning disable S2436 // Reduce the number of generic parameters in the 'CompositeSpecification' class to no more than the 2 authorized.
     internal abstract class CompositeSpecification<TExpression, TEntity, TKey> : Specification<TEntity, TKey>
+#pragma warning restore S2436 // Reduce the number of generic parameters in the 'CompositeSpecification' class to no more than the 2 authorized.
         where TExpression : Expression
         where TEntity : class, IEntity<TKey>
     {
-        internal CompositeSpecification(Specification<TEntity, TKey> left, Specification<TEntity, TKey> right)
+        private protected CompositeSpecification(Specification<TEntity, TKey> left, Specification<TEntity, TKey> right)
         {
             Left = left;
             Right = right;
