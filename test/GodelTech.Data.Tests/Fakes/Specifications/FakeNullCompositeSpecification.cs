8df7b0709a70d8644bbb4399b00ef12250480c74
Vladimir Rodchenko <vladimir@rodchenko.by>
2023-08-17 16:16:30 +0200

    #0 solve Warning CA1852: Type 'FakeNullCompositeSpecification' can be sealed

--- test/GodelTech.Data.Tests/Fakes/Specifications/FakeNullCompositeSpecification.cs
+++ test/GodelTech.Data.Tests/Fakes/Specifications/FakeNullCompositeSpecification.cs
@@ -3,7 +3,7 @@ using GodelTech.Data.Specifications;
 
 namespace GodelTech.Data.Tests.Fakes.Specifications
 {
-    internal class FakeNullCompositeSpecification<TExpression, TEntity, TKey> : CompositeSpecification<TExpression, TEntity, TKey>
+    internal sealed class FakeNullCompositeSpecification<TExpression, TEntity, TKey> : CompositeSpecification<TExpression, TEntity, TKey>
         where TExpression : Expression
         where TEntity : class, IEntity<TKey>
     {
